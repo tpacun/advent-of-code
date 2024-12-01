24687402792378de696e40c704571097fa705f93
Theo <62911461+tpacun@users.noreply.github.com>
2024-12-01 11:41:02 -0500

    Day one part two

--- 2024/day1/day1.js
+++ 2024/day1/day1.js
@@ -4,15 +4,42 @@ const rawInput = fs.readFileSync('./input.txt', 'utf-8')
 const arrLines = rawInput.split('\n')
 
 // Part one
-const listOne = arrLines.map((str) => str.split('   ')[0]).sort((a, b) => b - a)
-const listTwo = arrLines.map((str) => str.split('   ')[1]).sort((a, b) => b - a)
+const listOne = arrLines.map((str) => parseInt(str.split('   ')[0])).sort((a, b) => b - a)
+const listTwo = arrLines.map((str) => parseInt(str.split('   ')[1])).sort((a, b) => b - a)
 
 let diffSum = 0
 
 for (let i = 0; i < listOne.length; i++) {
-    let diff = parseInt(listOne[i]) - parseInt(listTwo[i])
+    let diff = listOne[i] - listTwo[i]
     diffSum += Math.abs(diff)
 }
-console.log(diffSum)
+
+console.log(`Part One: ${diffSum}`)
 
 // Part Two
+
+const hashOne = {}
+const hashTwo = {}
+
+for (let i = 0; i < listOne.length; i++) {
+    if (hashOne[listOne[i]]) {
+        hashOne[listOne[i]] += 1
+    } else {
+        hashOne[listOne[i]] = 1
+    }
+
+    if (hashTwo[listTwo[i]]) {
+        hashTwo[listTwo[i]] += 1
+    } else {
+        hashTwo[listTwo[i]] = 1
+    }
+}
+
+let simScore = 0
+for (const [key, value] of Object.entries(hashOne)) {
+    if (hashTwo[key]) {
+        simScore += (parseInt(key) * value * hashTwo[key])
+    }
+}
+
+console.log(`Part Two: ${simScore}`)
