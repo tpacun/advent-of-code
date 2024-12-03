dc53acdcce1536ff0f7c62cf0dde95570e2dde35
Theo <62911461+tpacun@users.noreply.github.com>
2024-12-03 15:41:20 -0500

    Day Three part two

--- 2024/day3/day3.js
+++ 2024/day3/day3.js
@@ -2,16 +2,42 @@ const fs = require('node:fs')
 
 const rawInput = fs.readFileSync('./input.txt', 'utf-8')
 
+class CorruptedMem {
+
+    multipliedSum(input) {
+        let sum = 0
+        const regex = /mul\((\d{1,3}),(\d{1,3})\)/g
+        
+        let match;
+        while ((match = regex.exec(input)) !== null) {
+            sum += (parseInt(match[1]) * parseInt(match[2]))
+        }
+        return sum
+    }
+
+    conditionalSum(input) {
+        const donts = input.split("don\'t()")
+
+        const cleaned = donts.map((str, i) => {
+            if (i === 0) {return str}
+            const doStart = str.indexOf("do()")
+            if(doStart > -1) {
+                return str.slice(doStart)
+            }
+        })
+        const cleanedStr = cleaned.join('')
+        return this.multipliedSum(cleanedStr)
+
+    }    
+    
+}    
+
 // Part One
 
-let sum = 0;
+const corrupt = new CorruptedMem()
 
-const regex = /mul\((\d{1,3}),(\d{1,3})\)/g
-
-while ((match = regex.exec(rawInput)) !== null) {
-    sum += (parseInt(match[1]) * parseInt(match[2])
-)}
-
-// console.log(sum)
+console.log(corrupt.multipliedSum(rawInput))
 
 // Part Two
+
+console.log(corrupt.conditionalSum(rawInput))
